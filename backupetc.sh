0b63aea5a57159909830cb61a900661d6dc576c2
JTsmileY <hlyjt2000@163.com>
2023-03-23 10:33:33 +0800

    2023/3/23 1

--- backupetc.sh
+++ backupetc.sh
@@ -4,7 +4,7 @@ set -x
 
 cp ~/.bashrc ~/myetc/.bashrc
 cp ~/.vimrc ~/myetc/.vimrc
-cp ~/.xinitrc ~/myetc/.xinitrc
+# cp ~/.xinitrc ~/myetc/.xinitrc
 
 set +x
 echo "back over"
---
+++ gitconfig.sh
@@ -0,0 +1,2 @@
+git config --global user.name "JTsmileY"
+git config --global user.email "hlyjt2000@163.com"
